f7c47f326bd6b8a67eefe1b844ae0cf903c8c949
Qzaam <qzaam4u@gmail.com>
2026-08-09 03:25:25 +0530

    Update VendorCreateCampaign.jsx

--- src/pages/VendorCreateCampaign.jsx
+++ src/pages/VendorCreateCampaign.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import api from "../../utils/api";
+import api from "../utils/api";
 
 export default function VendorCreateCampaign() {
 
