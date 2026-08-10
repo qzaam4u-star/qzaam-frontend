165ff2a9a20b36d5f2891bb187ffa9001982cb8f
Qzaam <qzaam4u@gmail.com>
2026-08-10 12:25:27 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -95,7 +95,7 @@ function CampaignEntry() {
 
   return <Navigate to="/" replace />;
 }
-function AppLayout() {
+{*/function AppLayout() {
   const location = useLocation();
 
   const isCampaignPage = location.pathname.startsWith("/campaigns");
@@ -113,7 +113,7 @@ function AppLayout() {
       <Footer />
     </>
   );
-}
+} */}
 export default function App() {
   return (
     <ThemeProvider>
